841804daca8932554d2fc90333a06690f6b986a2
cvalente80 <42539540+cvalente80@users.noreply.github.com>
2019-02-07 11:01:58 +0000

    primeiro commit

---
+++ TestDrivenBook/src/testJunit/com/TestTemplate.java
@@ -0,0 +1,17 @@
+package testJunit.com;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class TestTemplate {
+	
+	@Test
+	public void oneVariale() throws Exception {
+		
+		Template 
+		
+	}
+	
+	
+
+}
